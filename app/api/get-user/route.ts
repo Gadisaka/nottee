ef144ccb6a3317f7a0825dc3d8de4f02ea9fa6f7
gadisa kasaye <gadisaka04@gmail.com>
2025-02-27 17:20:22 +0300

    Refactor user model and update user retrieval logic

--- app/api/get-user/route.ts
+++ app/api/get-user/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export const GET = async () => {
   try {
-    const users = await prisma.users.findMany();
+    const users = await prisma.user.findMany();
     return new NextResponse(JSON.stringify({ message: users, status: 200 }));
   } catch (err) {
     console.log(err);
